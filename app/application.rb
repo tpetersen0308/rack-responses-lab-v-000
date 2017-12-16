112f38ee096f68d8274174b3dd55742097a3478c
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-16 01:55:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,11 @@
 class Application
 
-  
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.new.hour < 12 ? resp.write "Good Morning!" : resp.write "Good Afternoon!"
+
+    resp.finish
+  end
 
 end
