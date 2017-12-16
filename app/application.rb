96110da9d4152cbaa96fd917ce7ebac209540f93
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-16 02:04:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     t = Time.new
-    if "#{t.hour}:#{t.min}:#{t.sec}" < "12:0:0"
+    if "#{t.hour}:#{t.min}:#{t.sec}" < "12:0"
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
